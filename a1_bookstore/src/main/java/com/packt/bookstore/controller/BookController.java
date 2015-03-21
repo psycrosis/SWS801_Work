0964f5bd517738e48d807ef63cd995270ddcdc0b
psycrosisLAP <spooner@outlook.com>
2015-03-21 12:47:10 -0400

    push

---
+++ a1_bookstore/src/main/java/com/packt/bookstore/controller/BookController.java
@@ -0,0 +1,13 @@
+package com.packt.bookstore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+@RequestMapping("/books")
+public class BookController 
+{
+
+}
--- a1_bookstore/src/main/java/com/packt/bookstore/controller/HomeController.java
+++ a1_bookstore/src/main/java/com/packt/bookstore/controller/HomeController.java
@@ -7,22 +7,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController 
 {
+///////////////////////
 	@RequestMapping("/")
 	public String welcome(Model model)
 	{
-		model.addAttribute("greeting","Enter the Book Depot");
-		
-		// stuff to populate grid here ???
-		//
-		
+		model.addAttribute("greeting","The Book Depot");
+		model.addAttribute("entryLink", "Click to Enter");
 		return "forward:/welcome/greeting";
 	}
 	
 	@RequestMapping("/welcome/greeting")
-	public String greeting() {
+	public String greeting() 
+	{
 		return "welcome";
 	}
-		
-		
-	
+	//////////////////////////////
 }
